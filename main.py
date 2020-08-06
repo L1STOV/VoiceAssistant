bfbf6a8d4e1c06425eb5ee6c671e4475caa4fc04
L1STOV <listovspartak99@gmail.com>
2020-08-07 00:47:36 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,103 @@
+import os
+import time
+import speech_recognition as sr
+from fuzzywuzzy import fuzz
+import pyttsx3
+import datetime
+import random
+import webbrowser
+
+# настройки
+opts = {
+    "alias": ('юля', 'юлечка', 'юленька', 'юла', 'джулия', 'юлька'),
+    "tbr": ('скажи', 'расскажи', 'покажи', 'сколько', 'произнеси'),
+    "cmds": {
+        "ctime": ('текущее время', 'сейчас времени', 'который час'),
+        "radio": ('включи музыку', 'воспроизведи радио', 'включи радио'),
+        "stupid1": ('расскажи анекдот', 'рассмеши меня', 'ты знаешь анекдоты'),
+        "search": ('искать', 'искать в гугл', 'ищи', 'ищи в гугл', 'найди', 'найди в гугл', 'поиск', 'поиск в гугл')
+    }
+}
+
+
+# функции
+def speak(what):
+    print(what)
+    speak_engine.say(what)
+    speak_engine.runAndWait()
+
+
+def callback(recognizer, audio):
+    try:
+        voice = recognizer.recognize_google(audio, language="ru-RU").lower()
+        print("[log] Распознано: " + voice)
+
+        if voice.startswith(opts["alias"]):
+            cmd = voice
+
+            for x in opts['alias']:
+                cmd = cmd.replace(x, "").strip()
+
+            for x in opts['tbr']:
+                cmd = cmd.replace(x, "").strip()
+
+            # распознаем и выполняем команду
+            cmd = recognize_cmd(cmd)
+            execute_cmd(cmd['cmd'])
+
+    except sr.UnknownValueError:
+        print("[log] Голос не распознан!")
+    except sr.RequestError as e:
+        print("[log] Неизвестная ошибка, проверьте интернет!")
+
+
+def recognize_cmd(cmd):
+    RC = {'cmd': '', 'percent': 0}
+    for c, v in opts['cmds'].items():
+
+        for x in v:
+            vrt = fuzz.ratio(cmd, x)
+            if vrt > RC['percent']:
+                RC['cmd'] = c
+                RC['percent'] = vrt
+
+    return RC
+
+
+def execute_cmd(cmd):
+
+    if cmd == 'ctime':
+        # сказать текущее время
+        now = datetime.datetime.now()
+        speak("Сейчас " + str(now.hour) + ":" + str(now.minute))
+
+    elif cmd == 'radio':
+        # воспроизвести радио
+        os.system("D:\\Jarvis\\res\\radio_record.m3u")
+
+    elif cmd == 'stupid1':
+        # Анекдоты ||| Чтение рандомной строки из файла, кодировка отвечает за чтение кириллицы
+        jokes = random.choice(list(open('jokes.txt', encoding="utf-8")))
+        speak(jokes)
+
+    elif cmd == 'search':
+        print("Секунду")
+
+    else:
+        print('Команда не распознана, повторите!')
+
+
+# запуск
+r = sr.Recognizer()
+m = sr.Microphone(device_index=1)
+
+with m as source:
+    r.adjust_for_ambient_noise(source)
+
+speak_engine = pyttsx3.init()
+
+speak("Привет, котик")
+speak("Я слушаю")
+stop_listening = r.listen_in_background(m, callback)
+while True:
+    time.sleep(0.1)  # infinity loop
